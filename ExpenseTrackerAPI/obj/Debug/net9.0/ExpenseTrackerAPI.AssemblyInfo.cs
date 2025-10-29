f3dfe62ab5b6f17e23d397d0c6544d4ef571a465
Your Name <your-email@example.com>
2025-10-29 12:06:14 +0530

    fifth commit , removing existing admin dashboard codes

--- ExpenseTrackerAPI/obj/Debug/net9.0/ExpenseTrackerAPI.AssemblyInfo.cs
+++ ExpenseTrackerAPI/obj/Debug/net9.0/ExpenseTrackerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65d36279bd63d323a4e726edb4f924ebc62092dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879110c872dc94017e5c5dbe4531d7137a882a4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
